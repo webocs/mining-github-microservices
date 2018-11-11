c5f493b8d6363aceacbc62ad04c7bc9b9cd3870a
Germán Pablo Del Tedesco <gpdeltedesco@emptybox.com.ar>
2018-11-11 18:34:56 -0300

    Updated query to 2018, new timespan: 2018-01-01 to 2018-10-31.
    Also:
     - Adapted top comment, and fixed explanation about filtering (we are 
    not filtering for repo creation date). Updated processing quota usage.
     - Used `JSON_EXTRACT_SCALAR` instead of raw `JSON_EXTRACT`.
     - Moved text filtering to first `SELECT` statement.
     - Fixed first `SELECT` statement grouping, to avoid duplicates.
     
     This query can be reworked to use only one `SELECT`, but we are too 
    close to the processing limit to do any testing.

--- googleBigQueryFirstFilter.sql
+++ googleBigQueryFirstFilter.sql
@@ -1,51 +1,42 @@
 -- This is the first applied filter. It was used to create the "firstFilterRepositories.json" file
 -- Selects repositories ID, name and URL for
--- * All Github Repositories that were created between a 7 months window  2017/01/01 and 2017/08/29 which:
---   * Have at least one tag create in that time window containing the word "microservice"
---   * Have at least 50 push events in that time window
+-- * All GitHub Repositories that, in a time peridod between 2018-01-01 and 2018-10-31:
+--   * Have been tagged at least once
+--   * Have the text "microservice" in their description
+--   * Have at least 50 push events in the same time window
 --
--- WARNING: This query was created for Google Big query and GithubArchive database, it takes 500GB+ of processing
--- and returns duplicated repos that you should remove.
+-- WARNING: This query was created for Google Big query and GithubArchive database, it takes 900GB+ of processing.
 
 SELECT
-  created.repo.id,
-  created.repo.name,
-  created.repo.url
-FROM(
-      SELECT repo.id , payload,repo.name,repo.url
-              FROM
-              (
-                  TABLE_DATE_RANGE(
-                    [githubarchive:day.],
-                    TIMESTAMP('2017-01-01'),
-                    TIMESTAMP('2017-08-29')
-                   )
-               )
-      WHERE
-        type='CreateEvent' AND
-        JSON_EXTRACT(payload, '$.ref_type') ='"tag"'
-      GROUP BY repo.id,payload,repo.name,repo.url
-) as created
-JOIN
-(
-  SELECT repo.id, count(*) as pushevents
-  FROM (
-      TABLE_DATE_RANGE([githubarchive:day.],
-        TIMESTAMP('2017-01-01'),
-        TIMESTAMP('2017-08-29')
-      )
-  )
-  where type='PushEvent'
-  group by repo.name,repo.id
-  having count(*)>50
-  order by pushevents DESC
-)      as pushed
-ON
-  created.repo.id==pushed.repo.id
-WHERE  JSON_EXTRACT(created.payload, '$.description') LIKE "%microservice%"
-
-
-
-
-
-
+    created.repo.id,
+    created.repo_name,
+    created.repo_url
+FROM (
+    SELECT repo.id, LAST(repo.name) as repo_name, LAST(repo.url) as repo_url
+    FROM (
+        TABLE_DATE_RANGE(
+            [githubarchive:day.],
+            TIMESTAMP('2018-01-01'),
+            TIMESTAMP('2018-10-31')
+        )
+    )
+    WHERE type = 'CreateEvent'
+        AND JSON_EXTRACT_SCALAR(payload, '$.ref_type') = 'tag'
+        AND JSON_EXTRACT_SCALAR(payload, '$.description') LIKE '%microservice%'
+    GROUP BY repo.id
+) AS created
+JOIN (
+    SELECT repo.id, count(*) as pushevents
+    FROM (
+        TABLE_DATE_RANGE(
+            [githubarchive:day.],
+            TIMESTAMP('2018-01-01'),
+            TIMESTAMP('2018-10-31')
+        )
+    )
+    WHERE type = 'PushEvent'
+    GROUP BY repo.name, repo.id
+    HAVING count(*) > 50
+    ORDER BY pushevents DESC
+) AS pushed
+    ON created.repo.id = pushed.repo.id
